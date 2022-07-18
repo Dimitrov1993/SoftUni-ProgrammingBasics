b86f5b6383c48ac604c818a4eb07238c440612f2
Stanislav Dimitrov <106249944+Dimitrov1993@users.noreply.github.com>
2022-07-18 10:38:49 +0300

    Add files via upload

---
+++ 12 - Nested Loops - Exercise/cinemaTickets.js
@@ -0,0 +1,66 @@
+function cinemaTickets(input) {
+    let index = 0;
+    let command = input[index];
+    index++;
+    let standartCounter = 0;
+    let studentCounter = 0;
+    let kidCounter = 0;
+    let totalTicketCounter = 0;
+
+    while (command !== "Finish") {
+        let name = command;
+        let freeSpace = Number(input[index]);
+        index++;
+
+        let ticketType = input[index];
+        index++;
+        let ticketCounter = 0;
+
+        while (ticketType !== "End") {
+            ticketCounter++;
+            switch (ticketType) {
+                case "standard": standartCounter++; break;
+                case "student": studentCounter++; break;
+                case "kid": kidCounter++; break;
+            }
+
+            if (ticketCounter >= freeSpace) {
+                break;
+            }
+            ticketType = input[index];
+            index++;
+        }
+        totalTicketCounter += ticketCounter;
+        let res = ticketCounter / freeSpace * 100;
+        console.log(`${name} - ${res.toFixed(2)}% full.`);
+        command = input[index];
+        index++;
+    }
+
+    let studentTickets = studentCounter / totalTicketCounter * 100;
+    let standardTickets = standartCounter / totalTicketCounter * 100;
+    let kidTickets = kidCounter / totalTicketCounter * 100;
+    console.log(`Total tickets: ${totalTicketCounter}`);
+    console.log(`${studentTickets.toFixed(2)}% student tickets.`);
+    console.log(`${standardTickets.toFixed(2)}% standard tickets.`);
+    console.log(`${kidTickets.toFixed(2)}% kids tickets.`);
+};
+
+cinemaTickets(["Taxi",
+    "10",
+    "standard",
+    "kid",
+    "student",
+    "student",
+    "standard",
+    "standard",
+    "End",
+    "Scary Movie",
+    "6",
+    "student",
+    "student",
+    "student",
+    "student",
+    "student",
+    "student",
+    "Finish"]);
